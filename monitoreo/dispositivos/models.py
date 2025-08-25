88ce21495298a2072aae7cdc98e85c5b8b546bb8
EstebanAguirre1 <esteban.aguirre@inacapmail.cl>
2025-08-25 13:38:42 -0400

    ola

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.model):
+    ESTADO = [
+        ("ACTIVO", "Activo"),
+        ("INACTIVO", "Inactivo"),
+    ]
+
+    estado = models.CharField(max_length=10, choices=ESTADOS, default="ACTIVO")
+    created_at = models.DateTimeFielD(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+# Create your models here.    
+class Categoria(BaseModel):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class Zona(BaseModel):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class Dispositivo(BaseModel):
+    nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    zona = models.ForeignKey(Zona, on_delete=models.CASCADE)
+    consumo_maximo = models.IntergerField()
+
+    def __str__(self):
+        return self.nombre
+    
+class Medicion(BaseModel):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class Alerta(BaseModel):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
